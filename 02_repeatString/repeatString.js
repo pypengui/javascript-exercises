bc1feb3bb61510a2135b0ede2a687f5c299d5152
pypengui <ebubekircaki@yahoo.com>
2023-04-12 22:11:00 -0400

    commit before ssd change :)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(a,b) {
+    let str = '';
+if (b<0){
+    return "ERROR"} else {
+    for (let i = 0; i < b; i++) {
+      str +=a
+    };
+};
+    return str;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+        return "ERROR";
+  } else {
+    let sum = 0;
+    for (let i = Math.max(a, b); i >= Math.min(a, b); i--) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
